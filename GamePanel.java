1a54728057de5acd869fe4c6aac6d68937d606bf
James Timothy McCravy <jmccravy@ggc.edu>
2014-03-17 19:05:44 -0400

    Update gameplayer - Keylisteners
    Jump is now Z. Shoot is now C. Whether or not down is being held or not is now recorded.
    
    Currently useless keylisteners for 1 and 2 buttons to switch megaManType based on which was last pressed (1 for normal, 2 for twilightsparkle). Does not yet really do anything.

--- GamePanel.java
+++ GamePanel.java
@@ -1,12 +1,17 @@
 package startUp;
 
-import java.awt.Color;
+//import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Image;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 
 import aPackage.TileMap;
@@ -16,7 +21,7 @@ import aPackage.MoveableObjects.PrototypePlayer;
 public class GamePanel extends JPanel implements Runnable, KeyListener
 {
 	public static final int width = 400;
-	public static final int height = 400;
+	public static final int height = 200;
 
 	private Thread thread;
 	private boolean running;
@@ -24,7 +29,7 @@ public class GamePanel extends JPanel implements Runnable, KeyListener
 	private BufferedImage image;
 	private Graphics2D graphics;
 
-	private int fps = 30; // Frames per second
+	private int fps = 60; // Frames per second
 	private int targetTime = 1000 / fps; // How fast the game will preferably run
 
 	private TileMap tileMap;
@@ -104,8 +109,21 @@ public class GamePanel extends JPanel implements Runnable, KeyListener
 
 	private void render()
 	{
-		graphics.setColor(Color.BLACK);
-		graphics.fillRect(0, 0, width, height);
+
+		Image backgroundImg = null;
+		try
+		{
+			backgroundImg = ImageIO.read(new File(
+					"graphics/background_old_royal_castle.gif"));
+
+		} catch (IOException e)
+		{
+			System.out.println("The background doesn't exist");
+		}
+		graphics.drawImage(backgroundImg, 0, 0, null);
+
+		// graphics.setColor(Color.BLACK);
+		// graphics.fillRect(0, 0, width, height);
 
 		tileMap.draw(graphics);
 		player.draw(graphics);
@@ -134,13 +152,25 @@ public class GamePanel extends JPanel implements Runnable, KeyListener
 		{
 			player.setRight(true);
 		}
-		if(code == KeyEvent.VK_SHIFT)
+		if(code == KeyEvent.VK_DOWN)
+		{
+			player.setDown(true);
+		}
+		if(code == KeyEvent.VK_Z)
 		{
 			player.setJumping(true);
 		}
-		if(code == KeyEvent.VK_X)
+		if(code == KeyEvent.VK_C)
 		{
 			player.setWeaponUse(true);
+		}
+		if(code == KeyEvent.VK_1)
+		{
+			player.setMegaManType("normal");
+		}
+		if(code == KeyEvent.VK_2)
+		{
+			player.setMegaManType("twilightsparkle");
 		}
 	}
 
@@ -157,7 +187,11 @@ public class GamePanel extends JPanel implements Runnable, KeyListener
 		{
 			player.setRight(false);
 		}
-		if(code == KeyEvent.VK_X)
+		if(code == KeyEvent.VK_DOWN)
+		{
+			player.setDown(false);
+		}
+		if(code == KeyEvent.VK_C)
 		{
 			player.setWeaponUse(false);
 		}
