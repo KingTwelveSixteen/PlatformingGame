eeb2b5ebdf18ea092c3da12c8e2193d38e246563
James Timothy McCravy <jmccravy@ggc.edu>
2014-03-25 02:21:17 -0400

    Updated Tilemap - ERROR IN CODE
    For some reason the call to 'new Tile(currentTileImage ,false);' tries to go to 'aPackage.Tile.Tile', when it should go to 'aPackage.Tile'.
    
    No idea why.

--- TileMap.java
+++ TileMap.java
@@ -1,6 +1,7 @@
 package aPackage;
 
 import java.io.*;
+import java.util.ArrayList;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import javax.imageio.ImageIO;
@@ -13,20 +14,22 @@ public class TileMap
 	private int y;
 
 	private int tileSize;
-	private int[][] map;
+	private int[][] tileIDMap;
 	private int mapWidth;
 	private int mapHeight;
 
 	private BufferedReader reader;
-	private BufferedReader backgroundReader;
-	private String delimeter = "\\s+"; // Need to do this for numbers greater than 10 otherwise
-					   // it would read it as 1, then 0 and screw everything up.
-					   // \\s+ means any white space.
+	// private BufferedReader backgroundReader;
+	private String delimeter = "\\s+|\\n"; // Need to do this for numbers greater than 10
+	// otherwise
+	// it would read it as 1, then 0 and screw everything
+	// up.
+	// \\s+ means any white space.
 
-	private BufferedImage tileSet;
+	private BufferedImage fullTilesetImage;
 	private String tileSetLocation = "graphics/tilesets/";
 
-	private Tile tiles[][];
+	private ArrayList<Tile> tilesByID;
 
 	// These values are for the edge of the map: the screen will not scroll past these points.
 	private int minX;
@@ -42,71 +45,102 @@ public class TileMap
 		{
 			reader = new BufferedReader(new FileReader(tileMapName + ".txt"));
 
-			// The first two lines of the file MUST be the WIDTH, then HEIGHT of the
-			// map. The actual map comes afterwards.
+			// The first two lines of the file MUST be the WIDTH, then HEIGHT of the map. The actual
+			// map comes afterwards.
 			mapWidth = Integer.parseInt(reader.readLine());
 			mapHeight = Integer.parseInt(reader.readLine());
 
-			minX = GamePanel.width - mapWidth * tileSize;
-			minY = GamePanel.height - mapHeight * tileSize;
+			minX = GamePanel.width - (mapWidth * tileSize);
+			minY = GamePanel.height - (mapHeight * tileSize);
 
-			map = new int[mapHeight][mapWidth]; // Y, then X
+			tileIDMap = new int[mapHeight][mapWidth]; // Y, then X
+
+			// Creates an empty string array of the total size of the tileMap
+			String[] stringTilesArray = new String[mapWidth * mapHeight];
+			int tilesRead = 0;
+
+			// While not read as many tiles as the total number of tiles...
+			while(tilesRead < mapWidth * mapHeight)
+			{
+				// Read one more of the lines...
+				String currentLine = reader.readLine();
+
+				// Split it by the delimeter to get the tile numbers...
+				String[] singleTiles = currentLine.split(delimeter);
+
+				// Then add every number on the line to the array of tiles.
+				for(int i = 0; i < singleTiles.length; i++)
+				{
+					stringTilesArray[tilesRead] = singleTiles[i];
+
+					tilesRead++; // Remember to increase the counter of how many
+					// tiles have been read so far, or this goes on
+					// forever!
+				}
+				// Repeat as necessary. Thus, the while loop.
+			}
 
 			for(int row = 0; row < mapHeight; row++)
 			{
-				String currentLine = reader.readLine();
-				String[] tokens = currentLine.split(delimeter);
 
 				for(int column = 0; column < mapWidth; column++)
 				{
-					map[row][column] = Integer.parseInt(tokens[column]);
+					tileIDMap[row][column] = Integer.parseInt(stringTilesArray[column]);
 				}
 			}
 
-		} catch (Exception e)
+		} catch (FileNotFoundException e)
 		{
-			System.out.println("The tile-map file was not found.");
+			System.out.println("Tilemap not found exception in TileMap.");
+		} catch (NumberFormatException e)
+		{
+			System.out.println("Number format exception in TileMap constructor");
+		} catch (IOException e)
+		{
+			System.out.println("IO Exceeption in TileMap constructor");
 		}
 
 	}
 
-	public void loadTiles(String tileSetName, boolean onePixelOffset)
+	public void loadTiles(String tileSetName, int pixelOffset)
 	{
+
 		try
 		{
-			tileSet = ImageIO.read(new File(tileSetLocation + tileSetName));
+			fullTilesetImage = ImageIO.read(new File(tileSetLocation + tileSetName));
 
-			int numTilesAcross;
-			if(onePixelOffset)
+			int numTilesAcross = (fullTilesetImage.getWidth() / tileSize) + pixelOffset;
+			int numTilesDown = (fullTilesetImage.getHeight() / tileSize) + pixelOffset;
+
+			BufferedImage currentTileImage;
+			int currentTileID = 0;
+			for(int row = 0; row < numTilesDown; row++)
 			{
-				// If the tileset being used has 1 pixel of white-space squares
-				// surrounding all the tiles the +1's are gonna get rid of that.
-				numTilesAcross = (tileSet.getWidth() + 1) / (tileSize + 1);
+				for(int column = 0; column < numTilesAcross; column++)
+				{
+					currentTileImage = fullTilesetImage.getSubimage((column * tileSize)
+							+ pixelOffset, (row * tileSize) + pixelOffset, tileSize, tileSize);
+
+					// The prototype tileset stuff is set up so that all the blocked things are on a
+					// row other than the first. The top row contains all background elements
+					// (currently).
+					if(row == 0)
+					{
+						Tile currentTile = new Tile(currentTileImage, false);
+						tilesByID.set(currentTileID, currentTile);
+					}
+					else
+					{
+						Tile currentTile = new Tile(currentTileImage, true);
+						tilesByID.set(currentTileID, currentTile);
+					}
+					currentTileID++;
+				}
 			}
-			else
-			{
-				numTilesAcross = (tileSet.getWidth() / tileSize);
-			}
-
-			tiles = new Tile[2][numTilesAcross];
-
-			BufferedImage subImage;
-			for(int column = 0; column < numTilesAcross; column++)
-			{
-				subImage = tileSet.getSubimage(column * tileSize + column, 0,
-						tileSize, tileSize);
-				tiles[0][column] = new Tile(subImage, false);
-
-				// The prototype tileset stuff is set up so that all the blocked
-				// things are on the second row. Also only two available rows.
-				subImage = tileSet.getSubimage(column * tileSize + column,
-						tileSize + 1, tileSize, tileSize);
-				tiles[1][column] = new Tile(subImage, true);
-			}
-
-		} catch (Exception e)
+		} catch (IOException e)
 		{
-			System.out.println("tile load error");
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 	}
 
@@ -120,19 +154,19 @@ public class TileMap
 		return y;
 	}
 
-	public int getColumnTile(int x)
+	public int getColumnTileLocation(int x)
 	{
 		return x / tileSize;
 	}
 
-	public int getRowTile(int y)
+	public int getRowTileLocation(int y)
 	{
 		return y / tileSize;
 	}
 
-	public int getTile(int row, int column)
+	public int getTileIDAt(int row, int column)
 	{
-		return map[row][column];
+		return tileIDMap[row][column];
 	}
 
 	public int getTileSize()
@@ -140,14 +174,15 @@ public class TileMap
 		return tileSize;
 	}
 
-	public boolean isBlocked(int row, int column)
+	// The commented code is old stuff, it is kept because I don't remember why I did it that
+	// way, so there might be an actual reason to switch back.
+	public boolean isBlocked(int yLocation, int xLocation)
 	{
-		int currentTile = map[row][column];
+		// return tiles[row][column].isBlocked();
 
-		int r = currentTile / tiles[0].length;
-		int c = currentTile % tiles[0].length;
+		int checkingThisTileID = tileIDMap[yLocation][xLocation];
 
-		return tiles[r][c].isBlocked();
+		return tilesByID.get(checkingThisTileID).isBlocked();
 	}
 
 	public void setX(int num)
@@ -199,8 +234,7 @@ public class TileMap
 		 * if(currentTile == 0) { graphics.setColor(Color.BLACK); } if(currentTile == 1) {
 		 * graphics.setColor(Color.WHITE); }
 		 * 
-		 * graphics.fillRect(x + column * tileSize, y + row * tileSize, tileSize, tileSize);
-		 * } }
+		 * graphics.fillRect(x + column * tileSize, y + row * tileSize, tileSize, tileSize); } }
 		 */
 
 		// Same as above, but with tileset included.
@@ -208,13 +242,10 @@ public class TileMap
 		{
 			for(int column = 0; column < mapWidth; column++)
 			{
-				int currentTile = map[row][column];
+				int currentTileID = tileIDMap[row][column];
 
-				int r = currentTile / tiles[0].length;
-				int c = currentTile % tiles[0].length;
-
-				graphics.drawImage(tiles[r][c].getImage(), x + column * tileSize, y
-						+ row * tileSize, null);
+				graphics.drawImage(tilesByID.get(currentTileID).getImage(), x + column * tileSize,
+						y + row * tileSize, null);
 			}
 		}
 	}
